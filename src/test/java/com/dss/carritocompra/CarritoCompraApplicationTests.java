4fc67c443805707d4713e14bfaaf06cca7b8e493
JaviGP2002 <javigp2002@gmail.com>
2024-10-09 14:37:26 +0200

    :sparkles: Init repository

---
+++ src/test/java/com/dss/carritocompra/CarritoCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.carritocompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoCompraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
